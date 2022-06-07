478bd17890c5ccbba1bb9bd2c1dd17b28ea8f485
christianmartinroffey <c.martinroffey@gmail.com>
2022-06-07 10:32:18 +0000

    added navigation styling

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,21 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar navbar-dark bg-dark mb-3">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">Star Wars</span>
+					<img src="https://static-mh.content.disney.io/starwars/assets/navigation/sw_logo_stacked-336c62367939.png"></img>
 				</Link>
+				<div className="ml-auto ">
+					<Link to="/demo">
+						<button className="btn btn-success">Favourites 	&#8595; </button>
+					</Link>
+				</div>
+				<form class="d-flex">
+      				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+      					<button class="btn btn-outline-success" type="submit">Search</button>
+    				</form>
 			</div>
 		</nav>
 	);
