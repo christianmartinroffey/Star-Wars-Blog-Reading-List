199b46b538b4eaece17ef51d87fb97c60044cea5
christianmartinroffey <c.martinroffey@gmail.com>
2022-08-22 18:29:29 +0000

    continuing with the detailed view, returning html instead of JSON

--- src/js/views/PersonPage.js
+++ src/js/views/PersonPage.js
@@ -7,37 +7,44 @@ function PersonPage() {
   console.log(params.id, "params");
   const {store, actions} = useContext(Context);
   const people = store.people;
-  console.log(people, "Person page");
   const [details, setDetails] = useState();
-
   
     const person = people.filter(person => person.uid == params.id)
-    console.log(person, "name");
+    console.log(person, 'person obj');
 
-    const personUID = person.map((detail, index) => detail.uid)
-    console.log(personUID, "deets");
+    const personURL = person.map((detail, index) => detail.url)
+    const URLToString = personURL.toString();
+    console.log(personURL, URLToString, "person's URL");
     
   
-
-      fetch(`https://www.swapi.tech/api/people/${personUID}`)
+// useEffect(() => {
+     
+  fetch(URLToString, {
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    }
+  })
       .then(response => response.json())
-      .then(data => setDetails(data.results))
-      .then(()=> console.log(details, "person details"))
-      .catch(err => console.error(err.message))
+      .then(data => setDetails(data.result.properties))
+      .then(console.log(details, "person details"));
+
+// });
 
 
 
   return (
     <div>
-      {person.map((details, index) => (
+      <p>Link to URL <a href={personURL}>here</a></p>
+    
         <div>
           <p> Name: {details.name}</p>
           <p> ID: {details.uid}</p>
-          <p> Heigh: {details.height}</p>
+          <p> Height: {details.height}</p>
           <p> Mass: {details.mass}</p>
           <p> Hair Color: {details.hair_color}</p>
         </div>
-      ))}
+  
        
       <p> The individual person's info</p> 
     </div>
